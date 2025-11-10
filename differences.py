ae09abf29ed9eaddd39a3e34afd84de238c1277d
maxjing97 <maxjing97@gmail.com>
2025-11-10 16:57:58 -0500

    initial commit

---
+++ differences.py
@@ -0,0 +1,7 @@
+{'Biological_structure': 348, 'Sign_symptom': 937, 'Detailed_description': 326, 'Duration': 164, 'Severity': 86, 'Nonbiological_location': 191, 'Disease_disorder': 389, 'Coreference': 44, 'Clinical_event': 114, 'Subject': 137, 'Date': 134, 'Activity': 91, 'Medication': 470, 'Lab_value': 177, 'Outcome': 19, 'Frequency': 55, 'Age': 61, 'Dosage': 59, 'Diagnostic_procedure': 316, 'Time': 54, 'Texture': 4, 'History': 87, 'Therapeutic_procedure': 91, 'Sex': 6, 'Quantitative_concept': 11, 'Administration': 23, 'Other_event': 8, 'Occupation': 13, 'Distance': 5, 'Other_entity': 7, 'Color': 3, 'Family_history': 7, 'Personal_background': 3}
+
+{'Biological_structure': 260, 
+'Sign_symptom': 623, 
+'Disease_disorder': 481, 
+'Medication': 442, 
+'Detailed_description': 427, 'Date': 25, 'Clinical_event': 200, 'Lab_value': 176, 'Duration': 73, 'Diagnostic_procedure': 430, 'Severity': 53, 'Activity': 264, 'Administration': 49, 'Therapeutic_procedure': 180, 'Frequency': 26, 'Coreference': 35, 'Family_history': 10, 'Nonbiological_location': 139, 'Subject': 47, 'Time': 13, 'History': 39, 'Other_event': 15, 'Age': 12, 'Dosage': 24, 'Outcome': 7, 'Sex': 4, 'Other_entity': 13, 'Occupation': 3, 'Distance': 6, 'Quantitative_concept': 1, 'Texture': 2, 'Personal_background': 3}
---
+++ info.md
@@ -0,0 +1,7 @@
+## The Med Dialogue dataset: 
+
+About and using Bert: https://huggingface.co/datasets/UCSD26/medical_dialog 
+
+Download and installation link:    
+https://www.kaggle.com/datasets/xuehaihe/medical-dialogue-dataset/data  
+
